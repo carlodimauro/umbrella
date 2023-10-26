b9557010de9872a5329acf7dca9ed96c459d64e0
carlodimauro <146589712+carlodimauro@users.noreply.github.com>
2023-10-26 21:08:33 +0000

    done

--- umbrella.rb
+++ umbrella.rb
@@ -15,8 +15,23 @@ first_result = results.at(0)
 geo = first_result.fetch("geometry")
 loc = geo.fetch("location")
 
-lat = loc.fetch("lat")
-lon = loc.fetch("lng")
+lat = loc.fetch("lat").to_s
+lon = loc.fetch("lng").to_s
 
 
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_API_KEY")
+
+weather_url = "https://api.pirateweather.net/forecast/" +  ENV.fetch("PIRATE_WEATHER_API_KEY")+ "/" + lat +"," + lon
+
+
+weather_response = HTTP.get(weather_url)
+parsed_wresponse = JSON.parse(weather_response)
+
+
+current_stats = parsed_wresponse.fetch("currently")
+
+temperature_current = current_stats.fetch("temperature")
+temperature_current_c = (temperature_current - 32.0)*(5.0 /9.0)
+
+
+pp "The temperature in " + user_location.gsub("%20", " ") + " is currently " + temperature_current_c.round(2).to_s + " deg C"
